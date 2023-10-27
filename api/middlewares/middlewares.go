d1f39d8bc2c1fa6444af8c1fe328e5fea8044edf
lsortudo <leonardo.alvirubro@hotmail.com>
2023-10-27 01:05:17 -0300

    feat(middlewares): implement JSON and authentication middlewares (validity)

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -1,1 +1,27 @@
 package middlewares
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/lsortudo/blog-golang/api/auth"
+	"github.com/lsortudo/blog-golang/api/responses"
+)
+
+func setMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+		next(w, r)
+	}
+}
+
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := auth.TokenValid(r)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+			return
+		}
+		next(w, r)
+	}
+}
